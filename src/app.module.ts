8ef4c74d304106b9b40acbe62d7a5aefe1277b07
Islam412 <islam.124.hamdy@gmail.com>
2026-06-26 20:36:40 +0300

    prisma service file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TenantModule } from './modules/tenant/tenant.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TenantModule, UsersModule, AuthModule],
+  imports: [TenantModule, UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
